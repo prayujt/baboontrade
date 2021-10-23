a8da49bcd620bff1412fb8cbdb67df03e4611eb5
Prayuj Tuli <prayujtuli@hotmail.com>
2021-10-22 21:01:37 -0400

    Boilerplate socket.io code

---
+++ src/server.js
@@ -0,0 +1,29 @@
+let app = require('express')();
+let http = require('http').Server(app);
+const { MongoClient } = require('mongodb');
+
+const { connect } = require('./global');
+
+const io = require('socket.io')(http, {
+	cors: {
+		origin: 'http://localhost:2000',
+		methods: ['GET', 'POST'],
+	},
+});
+
+let port = 8000;
+
+connect('baboontrade', async (client) => {
+	io.on('connection', (socket) => {
+		console.log('user connected');
+		clientLobbyFunctions(client, socket);
+	});
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+http.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`);
+});
